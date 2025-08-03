7c4b0ab3198a2a84b6e3715b9d70354243adae50
ShacharShoshani <145845453+ShacharShoshani@users.noreply.github.com>
2025-08-03 13:33:24 +0300

    Feat: Login using an email and a password

--- app/src/main/java/shachar/afeka/course/volunteers/MainActivity.kt
+++ app/src/main/java/shachar/afeka/course/volunteers/MainActivity.kt
@@ -1,12 +1,16 @@
 package shachar.afeka.course.volunteers
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var helloWorldLabel: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViews()
+        initViews()
+
+    }
+
+    private fun initViews() {
+        val user = FirebaseAuth.getInstance().currentUser
+        if(user != null)
+            helloWorldLabel.text = buildString {
+                append("Hello ")
+                append(user.displayName)
+            }
+    }
+
+    private fun findViews() {
+        helloWorldLabel = findViewById(R.id.hello_world)
     }
 }
