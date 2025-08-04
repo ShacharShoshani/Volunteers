ef838171d287a55367b79ca4ec914512cf6b78aa
ShacharShoshani <145845453+ShacharShoshani@users.noreply.github.com>
2025-08-04 15:19:40 +0300

    Bugfix: User didn't moved to main activity after sign in

--- app/src/main/java/shachar/afeka/course/volunteers/LoginActivity.kt
+++ app/src/main/java/shachar/afeka/course/volunteers/LoginActivity.kt
@@ -59,14 +59,10 @@ class LoginActivity : AppCompatActivity() {
 
     private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
 //        val response = result.idpResponse
-//        if (result.resultCode == RESULT_OK) {
-//            // Successfully signed in
-//            val user = FirebaseAuth.getInstance().currentUser
-//        } else {
-//            // Sign in failed. If response is null the user canceled the
-//            // sign-in flow using the back button. Otherwise check
-//            // response.getError().getErrorCode() and handle the error.
-//            // ...
-//        }
+        if (result.resultCode == RESULT_OK) {
+            transactToMainActivity()
+        } else {
+            signIn()
+        }
     }
 }
