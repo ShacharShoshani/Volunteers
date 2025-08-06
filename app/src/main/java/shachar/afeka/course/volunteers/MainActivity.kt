941611ed488c2eaaf5857dcb62e5f17aec1f1ca0
ShacharShoshani <145845453+ShacharShoshani@users.noreply.github.com>
2025-08-06 17:54:49 +0300

    Feat: Display user's profile on the screen

--- app/src/main/java/shachar/afeka/course/volunteers/MainActivity.kt
+++ app/src/main/java/shachar/afeka/course/volunteers/MainActivity.kt
@@ -9,16 +9,26 @@ import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.launch
+import shachar.afeka.course.volunteers.utilities.DBClient
 import shachar.afeka.course.volunteers.utilities.SignalManager
 
 class MainActivity : AppCompatActivity() {
     private lateinit var _openMenuBtn: FloatingActionButton
     private lateinit var _navigationView: NavigationView
     private lateinit var _mainLayout: DrawerLayout
+
+    private lateinit var _name_input: TextInputEditText
+    private lateinit var _residence_input: TextInputEditText
+    private lateinit var _email_input: TextInputEditText
+    private lateinit var _phone_input: TextInputEditText
+
     private var user: FirebaseUser? = null
     private val firebaseAuth = FirebaseAuth.getInstance()
 
@@ -37,6 +47,11 @@ class MainActivity : AppCompatActivity() {
         user = firebaseAuth.currentUser
 
         initViews()
+
+        if (user != null)
+            lifecycleScope.launch {
+                loadUserProfile()
+            }
     }
 
     private fun initViews() {
@@ -75,5 +90,22 @@ class MainActivity : AppCompatActivity() {
         _openMenuBtn = findViewById(R.id.open_menu_btn)
         _navigationView = findViewById(R.id.nav_view)
         _mainLayout = findViewById(R.id.main)
+
+        _name_input = findViewById(R.id.username_text_input)
+        _residence_input = findViewById(R.id.residence_text_input)
+        _email_input = findViewById(R.id.email_text_input)
+        _phone_input = findViewById(R.id.phone_text_input)
+    }
+
+    private suspend fun loadUserProfile() {
+        val profile = DBClient.getInstance().getUserByUID(user!!.uid)
+
+        if (profile == null)
+            return
+
+        _name_input.setText(profile.name)
+        _residence_input.setText(profile.residence)
+        _email_input.setText(profile.email)
+        _phone_input.setText(profile.phone)
     }
 }
