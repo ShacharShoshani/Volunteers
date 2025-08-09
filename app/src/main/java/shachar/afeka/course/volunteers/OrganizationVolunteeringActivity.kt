b2cf48faba996ca11e32b4f72e06c8a4c0a2ca79
ShacharShoshani <145845453+ShacharShoshani@users.noreply.github.com>
2025-08-09 17:48:09 +0300

    Typo fix

--- app/src/main/java/shachar/afeka/course/volunteers/OrganizationVolunteeringActivity.kt
+++ app/src/main/java/shachar/afeka/course/volunteers/OrganizationVolunteeringActivity.kt
@@ -12,7 +12,7 @@ class OrganizationVolunteeringActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_organization_volunteerings)
+        setContentView(R.layout.activity_organization_volunteering)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
