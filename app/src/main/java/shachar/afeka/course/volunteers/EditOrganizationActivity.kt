7b34bdb210ef963162b194c87f54f543a775e439
ShacharShoshani <145845453+ShacharShoshani@users.noreply.github.com>
2025-08-10 11:50:22 +0300

    Create an abstract class for activities with map search capability

--- app/src/main/java/shachar/afeka/course/volunteers/EditOrganizationActivity.kt
+++ app/src/main/java/shachar/afeka/course/volunteers/EditOrganizationActivity.kt
@@ -1,94 +1,43 @@
 package shachar.afeka.course.volunteers
 
 import android.os.Bundle
-import android.widget.FrameLayout
-import android.widget.SearchView
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.libraries.places.api.Places
-import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.api.net.kotlin.searchByTextRequest
-import com.google.android.libraries.places.widget.PlaceSearchFragment
-import com.google.android.libraries.places.widget.PlaceSearchFragmentListener
-import com.google.android.libraries.places.widget.model.MediaSize
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.launch
-import shachar.afeka.course.volunteers.models.Coordinates
-import shachar.afeka.course.volunteers.ui.MapFragment
-import shachar.afeka.course.volunteers.utilities.MetaData
-import shachar.afeka.course.volunteers.utilities.Constants
+import shachar.afeka.course.volunteers.ui.FormWithMapSearchActivity
 import shachar.afeka.course.volunteers.utilities.DBClient
 import shachar.afeka.course.volunteers.utilities.SignalManager
-import java.lang.Exception
 
-class EditOrganizationActivity : AppCompatActivity() {
-    private lateinit var _mainFrameMap: FrameLayout
-    private lateinit var _mapFragment: MapFragment
-    private lateinit var _placeSearchFragment: PlaceSearchFragment
-    private lateinit var _placeSearch: SearchView
+class EditOrganizationActivity :
+    FormWithMapSearchActivity(
+        R.layout.activity_edit_organization,
+        R.id.main_FRAME_map,
+        R.id.place_search_fragment,
+        R.id.search_button
+    ) {
     private lateinit var _saveBtn: MaterialButton
     private lateinit var _nameInput: TextInputEditText
     private lateinit var _aboutInput: TextInputEditText
     private val _firebaseAuth = FirebaseAuth.getInstance()
     private var _user: FirebaseUser? = null
-    private var _location: LatLng =
-        LatLng(Coordinates.Default.lat, Coordinates.Default.lon)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_edit_organization)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         _user = _firebaseAuth.currentUser
-        findViews()
-        initViews()
     }
 
-    private fun findViews() {
-        _mainFrameMap = findViewById(R.id.main_FRAME_map)
-        _placeSearch = findViewById(R.id.search_button)
+    override fun findViews() {
+        super.findViews()
         _saveBtn = findViewById(R.id.organization_save_BTN)
         _nameInput = findViewById(R.id.organization_name_text_input)
         _aboutInput = findViewById(R.id.organization_about_text_input)
     }
 
-    private fun initViews() {
-        _mapFragment = MapFragment()
-        initSearchFragment()
-
-        _placeSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return true
-            }
-
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                if (query != null && !query.isEmpty()) {
-                    try {
-                        val search = searchByTextRequest(
-                            query, listOf(Place.Field.FORMATTED_ADDRESS)
-                        )
-
-                        _placeSearchFragment.configureFromSearchByTextRequest(search)
-                    } catch (err: Throwable) {
-                        SignalManager.getInstance().toast(err.toString())
-                    }
-                }
-
-                return true
-            }
-
-        })
+    override fun initViews() {
+        super.initViews()
 
         _saveBtn.setOnClickListener { _ ->
             if (_user != null) {
@@ -99,8 +48,8 @@ class EditOrganizationActivity : AppCompatActivity() {
                         DBClient.getInstance().addOrganization(
                             _nameInput.text.toString(),
                             _aboutInput.text.toString(),
-                            _location.latitude,
-                            _location.longitude,
+                            location.lat,
+                            location.lon,
                             _user!!.uid
                         )
                     } catch (err: Throwable) {
@@ -109,59 +58,11 @@ class EditOrganizationActivity : AppCompatActivity() {
                 }.invokeOnCompletion { finish() }
             }
         }
-
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.main_FRAME_map, _mapFragment)
-            .add(R.id.place_search_fragment, _placeSearchFragment)
-            .commit()
-    }
-
-    private fun initSearchFragment() {
-        val apiKey = MetaData.getGoogleGeoAPIKey(this.applicationContext)
-
-        if (apiKey == null)
-            throw Error("Google API key is missing.")
-
-        if (!Places.isInitialized()) {
-            Places.initialize(applicationContext, apiKey)
-        }
-        _placeSearchFragment =
-            PlaceSearchFragment.newInstance(PlaceSearchFragment.STANDARD_CONTENT)
-
-        _placeSearchFragment.preferTruncation = false
-        _placeSearchFragment.mediaSize = MediaSize.SMALL
-        _placeSearchFragment.selectable = true
-
-        _placeSearchFragment.registerListener(object : PlaceSearchFragmentListener {
-            override fun onLoad(places: List<Place>) {
-
-            }
-
-            override fun onRequestError(e: Exception) {
-
-            }
-
-            override fun onPlaceSelected(place: Place) {
-                super.onPlaceSelected(place)
-                val location: LatLng? = place.location
-
-                if (location != null) {
-                    _mapFragment.zoom(
-                        location.latitude,
-                        location.longitude,
-                        Constants.LocationDefault.ZOOM,
-                        place.displayName
-                    )
-                    _location = location
-                }
-            }
-        })
     }
 
     private fun updateControlsEnabledState() {
-        _placeSearch.isEnabled = !_placeSearch.isEnabled
-        _placeSearchFragment.selectable = !_placeSearchFragment.selectable
+        placeSearch.isEnabled = !placeSearch.isEnabled
+        placeSearchFragment.selectable = !placeSearchFragment.selectable
         _saveBtn.isEnabled = !_saveBtn.isEnabled
     }
 }
---
+++ app/src/main/java/shachar/afeka/course/volunteers/ui/FormWithMapSearchActivity.kt
@@ -0,0 +1,132 @@
+package shachar.afeka.course.volunteers.ui
+
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.SearchView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.kotlin.searchByTextRequest
+import com.google.android.libraries.places.widget.PlaceSearchFragment
+import com.google.android.libraries.places.widget.PlaceSearchFragmentListener
+import com.google.android.libraries.places.widget.model.MediaSize
+import shachar.afeka.course.volunteers.R
+import shachar.afeka.course.volunteers.models.Coordinates
+import shachar.afeka.course.volunteers.utilities.Constants
+import shachar.afeka.course.volunteers.utilities.MetaData
+import shachar.afeka.course.volunteers.utilities.SignalManager
+import java.lang.Exception
+
+abstract class FormWithMapSearchActivity(
+    val layoutId: Int,
+    val mapFrameId: Int,
+    val placeSearchFragmentContainerId: Int,
+    val placeSearchViewId: Int
+) :
+    AppCompatActivity() {
+    protected lateinit var mainFrameMap: FrameLayout
+    protected lateinit var mapFragment: MapFragment
+    protected lateinit var placeSearchFragment: PlaceSearchFragment
+    protected lateinit var placeSearch: SearchView
+    protected var location: Coordinates = Coordinates.Default
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(layoutId)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViews()
+        initViews()
+    }
+
+    protected open fun findViews() {
+        mainFrameMap = findViewById(mapFrameId)
+        placeSearch = findViewById(placeSearchViewId)
+    }
+
+    protected open fun initViews() {
+        mapFragment = MapFragment()
+        initSearchFragment()
+        initPlaceSearch()
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(mapFrameId, mapFragment)
+            .add(placeSearchFragmentContainerId, placeSearchFragment)
+            .commit()
+    }
+
+    private fun initPlaceSearch() {
+        placeSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null && !query.isEmpty()) {
+                    try {
+                        val search = searchByTextRequest(
+                            query, listOf(Place.Field.FORMATTED_ADDRESS)
+                        )
+
+                        placeSearchFragment.configureFromSearchByTextRequest(search)
+                    } catch (err: Throwable) {
+                        SignalManager.getInstance().toast(err.toString())
+                    }
+                }
+
+                return true
+            }
+
+        })
+    }
+
+    private fun initSearchFragment() {
+        val apiKey = MetaData.getGoogleGeoAPIKey(this.applicationContext)
+
+        if (apiKey == null)
+            throw Error("Google API key is missing.")
+
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, apiKey)
+        }
+        placeSearchFragment =
+            PlaceSearchFragment.newInstance(PlaceSearchFragment.STANDARD_CONTENT)
+
+        placeSearchFragment.preferTruncation = false
+        placeSearchFragment.mediaSize = MediaSize.SMALL
+        placeSearchFragment.selectable = true
+
+        placeSearchFragment.registerListener(object : PlaceSearchFragmentListener {
+            override fun onLoad(places: List<Place>) {
+
+            }
+
+            override fun onRequestError(e: Exception) {
+
+            }
+
+            override fun onPlaceSelected(place: Place) {
+                super.onPlaceSelected(place)
+                if (place.location != null) {
+                    mapFragment.zoom(
+                        place.location!!.latitude,
+                        place.location!!.longitude,
+                        Constants.LocationDefault.ZOOM,
+                        place.displayName
+                    )
+                    location =
+                        Coordinates(place.location!!.latitude, place.location!!.longitude)
+                }
+            }
+        })
+    }
+}
